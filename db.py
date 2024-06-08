65fbb22e568a6a99a570101817877e2d91b76853
devireddyprasanth22 <dpkrdrkr@gmail.com>
2024-06-08 13:06:47 +0800

    set up db

---
+++ db.py
@@ -0,0 +1,10 @@
+import sqlite3 as db
+
+def db_setup():
+    #create a usr databse and check if databae creation is successful
+    connection = db.connect("usr.db")
+    print(connection.total_changes)
+    # cursor object can send sql commands to database and table structure defined
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS acount (email TEXT NOT NULL, username TEXT, password TEXT NOT NULL, application TEXT NOT NULL)")
+    connection.commit()
