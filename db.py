6507a045499d4545a38db99b6dc95a2c78b5dcd7
devireddyprasanth22 <dpkrdrkr@gmail.com>
2024-06-09 12:36:08 +0800

    functionality to add new usr details complete

--- db.py
+++ db.py
@@ -6,10 +6,14 @@ def db_setup():
     print(connection.total_changes)
     # cursor object can send sql commands to database and table structure defined
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS acount (email TEXT NOT NULL, username TEXT, password TEXT NOT NULL, application TEXT NOT NULL)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS usr_details (username TEXT NOT NULL, email TEXT, password TEXT NOT NULL, application TEXT NOT NULL)")
     connection.commit()
 
-# def add_user():
+def add_user(username, email, password, application):
+    connection = db.connect("usr.db")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO usr_details (username, email, password, application) VALUES (?,?,?,?)", (username, email, password, application))
+    connection.commit()
 
 # def find_user():
 
