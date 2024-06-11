7fdaa1d05d4f0b21e8af1a5bbde735fc6b80df00
devireddyprasanth22 <dpkrdrkr@gmail.com>
2024-06-11 12:22:50 +0800

    working on secure passowrd input and encryption

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 # create a master password encryption key and handle encryption and decryption of data here
+import maskpass  # importing maskpass library
+
+pwd = maskpass.askpass(prompt="Password:", mask="#")
