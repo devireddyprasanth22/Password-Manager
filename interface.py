6758df1f6afc92f1c9312d60a7ab55e2519ceae5
devireddyprasanth22 <dpkrdrkr@gmail.com>
2024-06-14 12:08:36 +0800

    working on menu interface for user

--- interface.py
+++ interface.py
@@ -1,5 +1,14 @@
 # the interface that the user will see when this program is run
-# def options()
+
+def options():
+    print(('-'*6) + 'Menu'+ ('-' *6))
+    print('1. Add new account credentials')
+    print('2. Find account associated to email')
+    print('3. Change password for account')
+    print('4. Delete account credentials')
+    print('Q. Exit')
+    return input(': ')
+
 # def add_account() -> add_user from db
 # def find_account() -> find_usr(email) from db
 # def change_password() -> change_password(email, username, new password)
