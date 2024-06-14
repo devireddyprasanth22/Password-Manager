712617eac1e69b4864cac81041a8801c1ebf2856
devireddyprasanth22 <dpkrdrkr@gmail.com>
2024-06-14 11:57:00 +0800

    working on menu interface for user

---
+++ interface.py
@@ -0,0 +1,6 @@
+# the interface that the user will see when this program is run
+# def options()
+# def add_account() -> add_user from db
+# def find_account() -> find_usr(email) from db
+# def change_password() -> change_password(email, username, new password)
+# def del_account -> del_user(email, username, password)
