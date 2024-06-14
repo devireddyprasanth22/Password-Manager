4aa050cda9c7f279c20f3b5439556c974e0b71ce
devireddyprasanth22 <dpkrdrkr@gmail.com>
2024-06-14 12:29:16 +0800

    Creation of new accounts done

--- interface.py
+++ interface.py
@@ -2,6 +2,7 @@
 import maskpass 
 from db import add_user, find_user, change_password, del_user
 from password_gen import password_generator
+
 yes = {'yes','y', 'ye', ''}
 no = {'no','n'}
 def options():
@@ -20,14 +21,18 @@ def add_account(): #-> add_user from db
     email = input()
     print('Username for this application')
     username = input()
-    print('Do you already have a password for this application: [Y,n] ')
+    print('Do you already have a password for this application [Y,n]: ')
     choice = input()
     if choice in yes:
-        print('Enter your password: ')
-        password = input()
+        password = maskpass.askpass(prompt="Please enter your password:", mask="#")
     elif choice in no:
         password = password_generator()
+        print(f'Your secure password for this application is {password}')
+    add_user(username, email, password, application)
+    print('Account created!')
 
-# def find_account() -> find_usr(email) from db
+def find_account() #-> find_usr(email) from db
+    print('Please enter the email address: ')
+    email = input()
 # def change_password() -> change_password(email, username, new password)
 # def del_account -> del_user(email, username, password)
